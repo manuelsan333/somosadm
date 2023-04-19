5a999559bf7be1216ad0cfcd00968af1b98ed551
Alexander Padilla <yamop.16@gmail.com>
2023-04-19 08:27:14 -0600

    [UPDATE] added product specs

--- sh_pos_motorcycle/static/src/js/Screens/ProductScreen/ProductItem.js
+++ sh_pos_motorcycle/static/src/js/Screens/ProductScreen/ProductItem.js
@@ -19,15 +19,24 @@ odoo.define('sh_pos_motorcycle.ProductItem', function (require, factory) {
                 var self = this
                 const Product = event.detail
                 var motorcycle_id = Product.motorcycle_ids
+                var specification_line = Product.specification_lines
 
                 var details = []
+                var specs = []
 
                 _.each(motorcycle_id, function (each_motorcycle) {
                     var res = self.env.pos.db.get_motorcycle_by_id(each_motorcycle)
                     details.push(res)
                 })
+
+                _.each(specification_line, function (each_spec) {
+                    var res = each_spec.id
+                    specs.push(res)
+                })
                 this.showPopup('ProductDetailsPopup', {
-                    details: details
+                    details: details,
+                    product: Product.name,
+                    specs: specs,
                 })
 
             }
